871afe6e2ac7518cb5b57390ae05350c8a99952a
aankitsatpathy <113981163+aankitsatpathy@users.noreply.github.com>
2022-10-01 11:10:19 +0530

    Create acsii.c

---
+++ acsii.c
@@ -0,0 +1,15 @@
+/*find ascii value*/
+#include <stdio.h>
+
+main()
+{
+    char ch='a';
+    int i;
+    for(i=0;i<26;i++)
+    {
+        printf("\n ascii vlaue of %c=%d",ch,ch);
+        ch+=1;
+    }
+ 
+}
+    
