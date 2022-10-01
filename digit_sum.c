2f365995160024e942464ce3562feb122324f3e9
aankitsatpathy <113981163+aankitsatpathy@users.noreply.github.com>
2022-10-01 10:00:23 +0530

    Create digit_sum.c

---
+++ digit_sum.c
@@ -0,0 +1,21 @@
+/*find sum of digits*/
+#include <stdio.h>
+
+main()
+{
+    int a;
+    printf("enter a number");
+    scanf("%d",&a);
+    int b=a,c=0,sum=0;
+    while(b>0)
+    {
+        b/=10;
+        c++;
+    }
+    for(int d=0;d<c;d++)
+    {
+        sum+=(a%10);
+        a/=10;
+    }
+    printf("sum of digits= %d",sum);
+}
