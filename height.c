b418ac3e39a34056a3c9f85005d9b5048c2a01cd
aankitsatpathy <113981163+aankitsatpathy@users.noreply.github.com>
2022-10-01 07:42:50 +0530

    Create height.c

---
+++ height.c
@@ -0,0 +1,18 @@
+/*find average height of class*/
+#include <stdio.h>
+
+main()
+{
+  float sum=0,height[10];
+  printf("enter height of 10 students");
+  for(int a=0;a<10;a++)
+  {
+      scanf(" %f", &height[a]);
+  }
+  for (int a=0;a<10;a++)
+  {
+      sum+=height[a];
+  }
+  float avg=sum/10;
+  printf("average %f", avg);
+}
