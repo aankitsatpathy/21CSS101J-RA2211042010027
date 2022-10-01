6756b3bb0e2440c8082347a638cc08786c74fb3d
aankitsatpathy <113981163+aankitsatpathy@users.noreply.github.com>
2022-10-01 11:26:05 +0530

    Create mm to cm,m.c

---
+++ mm to cm,m.c
@@ -0,0 +1,13 @@
+/*mm to cm,m*/
+#include <stdio.h>
+
+main()
+{
+    float mm,cm,mtr;
+    printf("enter lenght in millimeter");
+    scanf("%f",&mm);
+    cm=mm/10;
+    mtr=mm/1000;
+    printf("%fmm in cm=%f\n ",mm,cm);
+    printf("%fmm in meter=%f\n",mm,mtr);
+}
