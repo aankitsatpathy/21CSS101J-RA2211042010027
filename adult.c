0215b3195a36f2fdb0ee15c2433c2800642bc233
aankitsatpathy <113981163+aankitsatpathy@users.noreply.github.com>
2022-10-04 22:01:25 +0530

    Create adult.c

---
+++ adult.c
@@ -0,0 +1,21 @@
+/*adult or not*/
+#include <stdio.h>
+
+int main()
+{
+    printf("enter age");
+    int a;
+    scanf("%d",&a);
+    if(a>18)
+    {
+        printf(" adult");
+    }
+    else
+    {
+        printf(" not adult");
+    }
+}
+/*output:
+enter age76
+ adult
+ */
