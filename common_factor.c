ab9a992a25bf9777f6b95fae6a8194dbab285333
aankitsatpathy <113981163+aankitsatpathy@users.noreply.github.com>
2022-10-04 22:47:23 +0530

    Create common_factor.c

---
+++ common_factor.c
@@ -0,0 +1,16 @@
+/*common factor*/
+#include <stdio.h>
+
+int main()
+{
+    printf("enter3number");
+    int a,b,c;
+    scanf("%d %d %d",&a,&b,&c);
+    for(int d=0;d>a;d++)
+    {
+        if(a%d==0&&b%d==0&&c%d==0)
+        {
+            printf("%d",d);
+        }
+    }
+}
