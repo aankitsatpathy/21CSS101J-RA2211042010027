d4691e9b19093cb5dffa4c8421b7b3529da43ee1
aankitsatpathy <113981163+aankitsatpathy@users.noreply.github.com>
2022-10-04 22:09:46 +0530

    Create last_digit_factor.c

---
+++ last_digit_factor.c
@@ -0,0 +1,22 @@
+/*check if last digit os the factor of 25*/
+#include <stdio.h>
+
+int main()
+{
+    printf("enter no.");
+    int a;
+    scanf("%d",&a);
+    int d=a%10;
+    if(25%d==0)
+    {
+        printf("true");
+    }
+    else
+    {
+        printf("false");
+    }
+}
+/*output:
+enter no.3456788885
+true
+ */
