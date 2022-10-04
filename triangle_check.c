a3537fc83227b66d15bb3d07c86d725bf8e33d72
aankitsatpathy <113981163+aankitsatpathy@users.noreply.github.com>
2022-10-04 21:34:20 +0530

    Create triangle_check.c

---
+++ triangle_check.c
@@ -0,0 +1,23 @@
+/*triangle check*/
+#include <stdio.h>
+
+int main()
+{
+    printf("enter three numbers");
+    int a,b,c;
+    scanf("%d %d %d",&b,&a,&c);
+    if((a+b)>c&&(b+c)>a&&(c+a)>b)
+    {
+        printf("triangle posible");
+    }
+    else
+    {
+        printf("not a triangle");
+    }
+}
+/*output:
+enter three numbers3
+4
+5
+triangle posible
+ */
