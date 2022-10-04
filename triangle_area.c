3627192fcc94c6bd07f1a0272933e03b1f2428d5
aankitsatpathy <113981163+aankitsatpathy@users.noreply.github.com>
2022-10-04 13:43:06 +0530

    Create triangle_area.c

---
+++ triangle_area.c
@@ -0,0 +1,15 @@
+/*triangle area*/
+#include <stdio.h>
+
+int main()
+{
+    printf("enter base and height");
+    int b,h;
+    scanf(" %d %d",&b,&h);
+    float a=0.5*(b*h);
+    printf("area = %f",a);
+}
+/*output:
+enter base and height23
+2
+area = 23.000000*/
