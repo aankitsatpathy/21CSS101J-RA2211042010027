0b87e10fcf1d78befe245213f263a25d42557669
aankitsatpathy <113981163+aankitsatpathy@users.noreply.github.com>
2022-10-04 18:23:45 +0530

    Create smallest.c

---
+++ smallest.c
@@ -0,0 +1,22 @@
+/*smallest number*/
+#include <stdio.h>
+
+int main()
+{
+    printf("enter 2umber");
+    int a,b;
+    scanf("%d %d",&a,&b);
+    if(a<b)
+    {
+        printf("smallest is=%d",a);
+    }
+   else
+   {
+       printf("smallest is=%d",b);
+   }
+}
+/*output:
+enter 2umber54
+45
+smallest is=45
+ */
