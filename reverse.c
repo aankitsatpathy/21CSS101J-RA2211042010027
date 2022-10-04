601f58b19c93d953cdacb62e72edd1b22f0a11a6
aankitsatpathy <113981163+aankitsatpathy@users.noreply.github.com>
2022-10-04 14:15:35 +0530

    Create reverse.c

---
+++ reverse.c
@@ -0,0 +1,20 @@
+/*digit reverse*/
+#include <stdio.h>
+
+int main()
+{
+    printf("enter a number");
+    int a,b,r=0;
+    scanf(" %d",&a);
+    b=a;
+    for(int c=b%10;b>0;)
+    {
+        r=r*10+c;
+        b/=10;
+        c=b%10;
+    }
+    printf("reverse of %d= %d",a,r);
+}
+/*output:
+enter a number123
+reverse of 123= 321*/
