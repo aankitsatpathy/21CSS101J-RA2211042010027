a1450490ca4d21211e2e91f4a71b779969652442
aankitsatpathy <113981163+aankitsatpathy@users.noreply.github.com>
2022-10-04 17:53:44 +0530

    Create time.c

---
+++ time.c
@@ -0,0 +1,20 @@
+/* timearranger*/
+#include <stdio.h>
+
+int main()
+{
+    printf("enter time in seconds");
+    int h,m,s;
+    scanf("%d",&s);
+    h=s/3600;
+    s%=3600;
+    m=s/60;
+    s%=60;
+    printf("hours=%d\n minutes%d\n seconds%d\n",h,m,s);
+}
+/*output:
+enter time in seconds7478
+hours=2
+ minutes4
+ seconds38
+ */
