f81d5fbe3d87eac36f597988ee26caa6c4f3c223
aankitsatpathy <113981163+aankitsatpathy@users.noreply.github.com>
2022-10-04 23:46:42 +0530

    Create week.c

---
+++ week.c
@@ -0,0 +1,41 @@
+/*find week day*/
+#include <stdio.h>
+
+main()
+{
+    printf("enter day number");
+    int a;
+    scanf("%d",&a);
+    switch(a)
+    {
+        case 1:
+         printf("monday");
+         break;
+        case 2:
+         printf("tuesday");
+         break;
+        case 3: 
+         printf("wednesday");
+         break;
+        case 4:
+         printf("thursday");
+         break;
+        case 5:
+         printf("friday");
+         break;
+        case 6:
+         printf("saturday");
+         break;
+        case 7:
+         printf("sunday");
+         break;
+        default:
+         printf("not valid response");
+         break;
+    }
+}
+
+/*output:
+enter day number6
+saturday
+*/
