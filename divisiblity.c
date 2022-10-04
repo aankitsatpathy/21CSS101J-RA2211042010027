b4c2488d2bd1a8bb15011de7c243c2b0d7a0656c
aankitsatpathy <113981163+aankitsatpathy@users.noreply.github.com>
2022-10-04 18:39:52 +0530

    Create divisiblity.c

---
+++ divisiblity.c
@@ -0,0 +1,21 @@
+/*divisiblity by 6*/
+#include <stdio.h>
+
+int main()
+{
+    printf("enter number");
+    int a;
+    scanf("%d",&a);
+    if(a%6==0)
+    {
+        printf("divisible by 6");
+    }
+    else
+    {
+        printf("not divisible by 6");
+    }
+}
+/*output:
+enter number72
+divisible by 6
+ */
