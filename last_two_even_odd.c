7b6cc51dd2de6811f7dd5d0201127b0ffabbd646
aankitsatpathy <113981163+aankitsatpathy@users.noreply.github.com>
2022-10-04 22:15:11 +0530

    Create last_two_even_odd.c

---
+++ last_two_even_odd.c
@@ -0,0 +1,22 @@
+/*check if last2digitsare odd or even*/
+#include <stdio.h>
+
+int main()
+{
+    printf("enter no.");
+    int a;
+    scanf("%d",&a);
+    int d=a%100;
+    if(d%2==0)
+    {
+        printf("even");
+    }
+    else
+    {
+        printf("odd");
+    }
+}
+/*output:
+enter no.987654
+even
+ */
