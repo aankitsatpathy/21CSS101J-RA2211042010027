9e78b929d3e5bc35a8b8a87f30b603cd989ef048
aankitsatpathy <113981163+aankitsatpathy@users.noreply.github.com>
2022-10-04 23:50:55 +0530

    Create season.c

---
+++ season.c
@@ -0,0 +1,32 @@
+/*find season*/
+#include <stdio.h>
+
+main()
+{
+    printf("enter month number");
+    int a;
+    scanf("%d",&a);
+    switch(a)
+    {
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+         printf("summer");
+         break;
+        case 5:
+        case 6:
+        case 7:
+        case 8:
+         printf("rainy");
+         break;
+        default:
+         printf("winter");
+         break;
+    }
+}
+
+/*output:
+enter month number7
+rainy
+*/
