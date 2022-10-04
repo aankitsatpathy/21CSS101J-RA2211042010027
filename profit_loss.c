f5e79c521f78f27453bb915edb50726fd5c9d2d4
aankitsatpathy <113981163+aankitsatpathy@users.noreply.github.com>
2022-10-04 19:00:58 +0530

    Create profit_loss.c

---
+++ profit_loss.c
@@ -0,0 +1,24 @@
+/*profit loss*/
+#include <stdio.h>
+
+int main()
+{
+    printf("enter sales price and cost price");
+    int a,b;
+    scanf("%d %d",&a,&b);
+    if(a>b)
+    {
+        int p=a-b;
+        printf("profit=%d",p);
+    }
+    else
+    {
+        int l=b-a;
+        printf("loss=%d",l);
+    }
+}
+/*output:
+enter sales price and cost price567
+675
+loss=108
+ */
