4d336d4346b96074d49340125e862e30883e1a3c
aankitsatpathy <113981163+aankitsatpathy@users.noreply.github.com>
2022-10-04 22:32:14 +0530

    Create single_multi_digit.c

---
+++ single_multi_digit.c
@@ -0,0 +1,23 @@
+/*single digit number or multidigit number*/
+#include <stdio.h>
+
+int main()
+{
+    printf("enter number");
+    int a;
+    scanf("%d",&a);
+    int b=a/10;
+    if(b!=0)
+    {
+        printf("multidigit number");
+    }
+    else
+    {
+        
+        printf("singledigit number");
+    }
+}
+/*output:
+enter number8
+singledigit number
+ */
