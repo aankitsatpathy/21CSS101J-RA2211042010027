40303fe00e0d975d84b11fe8285c179b1c06f3ec
aankitsatpathy <113981163+aankitsatpathy@users.noreply.github.com>
2022-10-04 18:15:09 +0530

    Create greaterthan50.c

---
+++ greaterthan50.c
@@ -0,0 +1,21 @@
+/*greater than 50*/
+#include <stdio.h>
+
+int main()
+{
+    printf("enter number");
+    int a;
+    scanf("%d",&a);
+    if(a>50)
+    {
+        printf("greater than 50");
+    }
+    else
+    {
+        printf("less than 50");
+    }
+}
+/*output:
+enter number87
+greater than 50
+ */
