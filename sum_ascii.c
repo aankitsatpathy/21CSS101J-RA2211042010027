e0f33978b717348ac4b17e4a7bbb5487af23594a
aankitsatpathy <113981163+aankitsatpathy@users.noreply.github.com>
2022-10-04 13:54:25 +0530

    Create sum_ascii.c

---
+++ sum_ascii.c
@@ -0,0 +1,15 @@
+/*sum of ascii value*/
+#include <stdio.h>
+
+int main()
+{
+    printf("enter two characters");
+    char a,b;
+    scanf(" %c %c",&a,&b);
+    int sum=a+b;
+    printf("sum= %d",sum);
+}
+/*output:
+enter two charactersa
+b
+sum= 195*/
