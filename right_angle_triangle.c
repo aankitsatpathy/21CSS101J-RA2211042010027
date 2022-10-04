cee61a5e4bb099d211e25c8deef001d723bc9713
aankitsatpathy <113981163+aankitsatpathy@users.noreply.github.com>
2022-10-04 21:55:23 +0530

    Create right_angle_triangle.c

---
+++ right_angle_triangle.c
@@ -0,0 +1,23 @@
+/*right angle triangle*/
+#include <stdio.h>
+
+int main()
+{
+    printf("enter three numbers");
+    int a,b,c;
+    scanf("%d %d %d",&b,&a,&c);
+    if((a*a+b*b)==c*c||(b*b+c*c)==a*a||(c*c+a*a)==b*b)
+    {
+        printf("right angle triangle");
+    }
+    else
+    {
+        printf("not a right angle triangle");
+    }
+}
+/*output:
+enter three numbers3
+4
+5
+right angle triangle
+ */
