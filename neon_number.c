6799ff4e4895988487e5f015230c16f7e4e34c42
aankitsatpathy <113981163+aankitsatpathy@users.noreply.github.com>
2022-10-05 10:56:23 +0530

    Create neon_number.c

---
+++ neon_number.c
@@ -0,0 +1,28 @@
+/*neon number*/
+#include <stdio.h>
+
+main()
+{
+    printf("enter a number");
+    int b;
+    scanf("%d",&b);
+    int sq=b*b;
+    int sum=0;
+    while(sq>0)
+    {
+        sum+=sq%10;
+        sq/=10;
+    }
+    if(sum==b)
+    {
+        printf("neon number");
+    }
+    else
+    {
+        printf(" not a neon number");
+    }
+}
+/*output:
+enter a number9
+neon number
+*/
