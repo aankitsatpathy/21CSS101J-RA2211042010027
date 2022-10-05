7405d02ac32bce8091ce73db4cfe696057601f4f
aankitsatpathy <113981163+aankitsatpathy@users.noreply.github.com>
2022-10-05 10:49:37 +0530

    Create digit_plus.c

---
+++ digit_plus.c
@@ -0,0 +1,26 @@
+/*add 1 to digits of an integer*/
+#include <stdio.h>
+
+main()
+{
+    printf("enter a number");
+    int b;
+    scanf(" %d",b);
+    int ni=0;
+    while(b>0)
+    {
+        int d=b%10;
+        printf("k");
+        if(d==9)
+        {
+            ni=ni*10;
+            printf("%d",ni);
+        }
+        else
+        {
+            ni=ni*10+d;
+            printf("%d",ni);
+        }
+    }
+    printf("new integer=%d",ni);
+}
