b8287e28c3e173318bdb9cc576dda7f07b909856
aankitsatpathy <113981163+aankitsatpathy@users.noreply.github.com>
2022-10-05 13:08:36 +0530

    Create pyramid7.c

---
+++ pyramid/pyramid7.c
@@ -0,0 +1,20 @@
+/*pyramid 7*/
+#include<stdio.h>
+main()
+{ 
+    for(int a=5;a>0;a--)
+    {
+        for(int b=5;b>=a;b--)
+        {
+            printf("%d  ",a);
+        }
+        printf("\n");
+    }
+}
+/*output:
+5  
+4  4  
+3  3  3  
+2  2  2  2  
+1  1  1  1  1   
+*/
