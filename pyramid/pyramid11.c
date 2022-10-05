07c1ee8ac5955c45c358cc2242a8ab5207490980
aankitsatpathy <113981163+aankitsatpathy@users.noreply.github.com>
2022-10-05 16:26:16 +0530

    Create pyramid11.c

---
+++ pyramid/pyramid11.c
@@ -0,0 +1,28 @@
+/*pyramid 11*/
+#include<stdio.h>
+main()
+{ 
+    int c=1;
+    for(int a=1;a<=4;a++)
+    {
+        for(int b=1;b<5;b++)
+        {
+            if(b>(5-a))
+            {
+                printf("%d   ",c);
+                c++;
+            }
+            else
+            {
+                printf("    ");
+            }
+        }
+        
+        printf("\n");
+    }
+}
+/*output:
+            1   
+        2   3   
+    4   5   6  
+*/
