bf9f958d456c4085b9bba6451f3e81da78fbd850
aankitsatpathy <113981163+aankitsatpathy@users.noreply.github.com>
2022-10-05 16:10:01 +0530

    Create pyramid8.c

---
+++ pyramid/pyramid8.c
@@ -0,0 +1,22 @@
+/*pyramid 8*/
+#include<stdio.h>
+main()
+{ 
+    int c=1;
+    for(int a=5;a>0;a--)
+    {
+        for(int b=1;b<=a;b++)
+        {
+            printf("%d  ",c);
+            c++;
+        }
+        printf("\n");
+    }
+}
+/*output:
+1  2  3  4  5  
+6  7  8  9  
+10  11  12  
+13  14  
+15  
+*/
