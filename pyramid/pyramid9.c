2fa209de481c44049480dccbd8ccc087d9dea744
aankitsatpathy <113981163+aankitsatpathy@users.noreply.github.com>
2022-10-05 16:17:31 +0530

    Create pyramid9.c

---
+++ pyramid/pyramid9.c
@@ -0,0 +1,28 @@
+/*pyramid 9*/
+#include<stdio.h>
+main()
+{ 
+    for(int a=1;a<=5;a++)
+    {
+        for(int b=1;b<6;b++)
+        {
+            if(b>(5-a))
+            {
+                printf("%d ",b);
+            }
+            else
+            {
+                printf("  ");
+            }
+        }
+        
+        printf("\n");
+    }
+}
+/*output:
+        5 
+      4 5 
+    3 4 5 
+  2 3 4 5 
+1 2 3 4 5 
+*/
