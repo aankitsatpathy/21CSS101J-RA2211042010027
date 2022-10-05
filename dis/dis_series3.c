3558c9a70103a3c4f0e292afd820c1e359eb71ce
aankitsatpathy <113981163+aankitsatpathy@users.noreply.github.com>
2022-10-05 16:07:21 +0530

    Create dis_series3.c

---
+++ dis/dis_series3.c
@@ -0,0 +1,19 @@
+/*display series 1,12,123,1234,12345....n*/
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("enter a number of terms");
+    scanf("%d",&n);
+    int a=0;
+    for(int b=1;b<=n;b++)
+    {
+        a=a*10+b;
+        printf("%d,",a);
+    }
+}
+/*output:
+enter a number of terms5
+1,12,123,1234,12345,
+*/
