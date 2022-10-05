895b285f143c0aef10f01678cc47c43bfadae89e
aankitsatpathy <113981163+aankitsatpathy@users.noreply.github.com>
2022-10-05 11:14:09 +0530

    Create evil_number.c

---
+++ evil_number.c
@@ -0,0 +1,34 @@
+/*evil number*/
+main()
+{  
+    
+    int a[10],n,c=0;  
+    printf("Enter the number");
+    scanf("%d",&n);
+    for(int i=0;n>0;i++)
+    {
+        a[i]=n%2;
+        n=n/2;
+        
+    }   
+    for(int i=0;i<n;i++)
+    {
+        if(a[i]==0)
+        {
+            c++;
+        }
+        
+    } 
+    if(c%2==0)
+    {
+        printf("evil number");
+    }
+    else
+    {
+        printf("not evil number");
+    }
+}
+/*output:
+Enter the number10
+evil number
+*/
