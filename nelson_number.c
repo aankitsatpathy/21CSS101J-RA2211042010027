e4b0f4389f685840d7f7b1147e321a1b96457c25
aankitsatpathy <113981163+aankitsatpathy@users.noreply.github.com>
2022-10-05 11:42:59 +0530

    Create nelson_number.c

---
+++ nelson_number.c
@@ -0,0 +1,31 @@
+/*nelson number*/
+main()
+{  
+    
+    int n,d=0;  
+    printf("Enter the number");
+    scanf("%d",&n);
+    int a=n,c=n%10;
+    while(a>0)
+    {
+        int e=a%10;
+        if(e!=c)
+        {
+            d++;
+        }
+        a/=10;
+    }
+      
+    if(d==0)
+    {
+        printf("nelson number");
+    }
+    else
+    {
+        printf(" not nelson number");
+    }
+}
+/*output:
+Enter the number888
+nelson number
+*/
