46eb69c282787bf4d9276348c552cb527d1a925c
aankitsatpathy <113981163+aankitsatpathy@users.noreply.github.com>
2022-10-05 11:37:37 +0530

    Create karpekar_number.c

---
+++ karpekar_number.c
@@ -0,0 +1,29 @@
+/*karpekar number*/
+main()
+{  
+    
+    int n;  
+    printf("Enter the number");
+    scanf("%d",&n);
+    int a=n,c=0;
+    while(n>0)
+    {
+        c++;
+        n/=10;
+    }
+    int p=(10,n-1);
+    int sq=n*n;
+    int r=(sq/p)+(sq%p);
+    if(r==n)
+    {
+        printf("karpekar number");
+    }
+    else
+    {
+        printf(" not karpekar number");
+    }
+}
+/*output:
+Enter the number297
+karpekar number
+*/
