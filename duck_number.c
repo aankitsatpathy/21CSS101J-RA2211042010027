01e78fa12503cdb369b7f1598b85d262c0212f22
aankitsatpathy <113981163+aankitsatpathy@users.noreply.github.com>
2022-10-05 11:01:00 +0530

    Create duck_number.c

---
+++ duck_number.c
@@ -0,0 +1,29 @@
+/*duck number*/
+#include <stdio.h>
+
+main()
+{
+    printf("enter a number");
+    int a=0,b;
+    scanf(" %d",&b);
+    while(b>0)
+    {
+        int d=b%10;
+        if(d==0)
+        {
+            a++;
+        }
+    }
+    if(a>0)
+    {
+        printf("duck number");
+    }
+    else
+    {
+        printf("not a duck number");
+    }
+}
+/*output:
+enter a number909
+duck number
+*/
