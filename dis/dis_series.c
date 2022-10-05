c7eaff51d931e5cc9cfc202165930090d11b0057
aankitsatpathy <113981163+aankitsatpathy@users.noreply.github.com>
2022-10-05 12:38:59 +0530

    Create dis_series.c

---
+++ dis/dis_series.c
@@ -0,0 +1,23 @@
+/*display series 1,3,7,15,31....n*/
+#include<stdio.h>
+main()
+{  
+    
+    int n,a=1;
+    printf("Enter the number");
+    scanf("%d",&n);
+    printf("%d\n",a);
+    for(int d=2;a<n;d*=2)
+    {
+       a+=d;
+       printf("%d\n",a);
+    }
+}
+/*output:
+Enter the number31
+1
+3
+7
+15
+31
+*/
