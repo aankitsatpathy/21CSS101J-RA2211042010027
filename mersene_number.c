4b4641aaf3b551510470159455fc37738d23b973
aankitsatpathy <113981163+aankitsatpathy@users.noreply.github.com>
2022-10-05 11:12:28 +0530

    Create mersene_number.c

---
+++ mersene_number.c
@@ -0,0 +1,34 @@
+/*mersene number*/
+main()
+{  
+    
+    int a[10],n,c=0;  
+    printf("Enter the number");
+    scanf("%d",&n);
+    for(int i=0;n>0;i++)
+    {
+        a[i]=n%2;
+        n=n/2;
+        
+    }   
+    for(int i=0;i<n;i++)
+    {
+        if(a[i]==0)
+        {
+            c++;
+        }
+        
+    } 
+    if(c==0)
+    {
+        printf("mersene number");
+    }
+    else
+    {
+        printf("not mersene number");
+    }
+}
+/*output:
+Enter the number7
+mersene number
+*/
