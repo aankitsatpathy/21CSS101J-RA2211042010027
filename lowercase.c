341776568834cacd57763101534da50b852286b5
aankitsatpathy <113981163+aankitsatpathy@users.noreply.github.com>
2022-10-10 23:35:45 +0530

    Update lowercase.c

--- lowercase.c
+++ lowercase.c
@@ -1,10 +1,14 @@
 /*lowercase*/
 #include <stdio.h>
-
 main()
 {
     char ch;
+    printf("Enter in uppercase: ");
     scanf("%c",&ch);
     int i;
-    printf("lowercase of  %c=%c",ch,ch+32);
+    printf("lowercase of  %c : %c",ch,ch+32);
 }
+/*output:
+Enter in uppercase: A
+lowercase of  A : a
+*/
