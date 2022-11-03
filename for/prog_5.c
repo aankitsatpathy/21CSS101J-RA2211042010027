61545ece806b5ce4783bb8596b52b198c35c7ffa
aankitsatpathy <113981163+aankitsatpathy@users.noreply.github.com>
2022-11-03 19:31:44 +0530

    Create prog_5.c

---
+++ for/prog_5.c
@@ -0,0 +1,11 @@
+//SUM OF SQUARE 1-10
+#include <stdio.h>
+
+int main()
+{
+    printf("RA2211042010027");
+    int s=0;
+   for(int i=1;i<11;i++)
+   s+=i*i;
+   printf("Sum of square = %d\n",s);
+}
