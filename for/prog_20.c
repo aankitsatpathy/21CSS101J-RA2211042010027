198b204c81cb7369bbdaa2dc7349910f3454ca80
aankitsatpathy <113981163+aankitsatpathy@users.noreply.github.com>
2022-11-03 21:05:12 +0530

    Create prog_20.c

---
+++ for/prog_20.c
@@ -0,0 +1,12 @@
+// odd
+#include <stdio.h>
+
+int main()
+{
+    printf("RA2211042010027\n");
+    int n1;
+    scanf("%d",&n1);
+    for(int a=1;a<n1;a++)
+    if(a%2!=0)
+    printf("%d\n",a);
+}
