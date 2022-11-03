d5ac73f1626258feeffa5e740462e7e6c3e1615f
aankitsatpathy <113981163+aankitsatpathy@users.noreply.github.com>
2022-11-03 21:09:57 +0530

    Create prog_22.c

---
+++ for/prog_22.c
@@ -0,0 +1,13 @@
+// even
+#include <stdio.h>
+
+int main()
+{
+    printf("RA2211042010027\n");
+    int n1,s=0;
+    scanf("%d",&n1);
+    for(int a=1;a<n1;a++)
+    if(a%2!=0)
+    s+=a;
+    printf("%d\n",s);
+}
