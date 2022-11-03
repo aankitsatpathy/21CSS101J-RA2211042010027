629103d29038508678873571888ee9bbd18791d0
aankitsatpathy <113981163+aankitsatpathy@users.noreply.github.com>
2022-11-03 20:49:50 +0530

    Create prog_14.c

---
+++ for/prog_14.c
@@ -0,0 +1,11 @@
+//multiplication table
+#include <stdio.h>
+
+int main()
+{
+    printf("RA2211042010027\n");
+    int n1;
+    scanf("%d",&n1);
+    for(int a=1;a<11;a++)
+    printf("%d*%d = %d\n",a,n1,a*n1);
+}
