23dedaa824c4cb7777f221c76c201e6cf1e7e6e0
aankitsatpathy <113981163+aankitsatpathy@users.noreply.github.com>
2022-11-07 02:14:20 +0530

    Create prog_33.c

---
+++ for/prog_33.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("RA2211042010027\n");
+    printf("Enter a number = ");
+    int a,s=0;
+    scanf("%d",&a);
+    for(;a>0;a/=10)
+    if(a%2==0)
+    s+=a%10;
+    printf("Sum of even digits = %d",s);
+}
