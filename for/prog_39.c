f6cc632728237a1b9e28e0757050c604330d0495
aankitsatpathy <113981163+aankitsatpathy@users.noreply.github.com>
2022-11-07 02:26:23 +0530

    Create prog_39.c

---
+++ for/prog_39.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("RA2211042010027\n");
+    printf("Enter a number = ");
+    int a,r=0;
+    scanf("%d",&a);
+    for(;a>0;a/=10){
+    int d=a%10;
+    if(a%2!=0)
+    r=r*10+(a%10);}
+    printf("New number = %d",r);
+    
+}
