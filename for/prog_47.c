f027e2dc9644ffbde853b6c0862f6ee5d82500d9
aankitsatpathy <113981163+aankitsatpathy@users.noreply.github.com>
2022-11-10 01:20:01 +0530

    Create prog_47.c

---
+++ for/prog_47.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("RA2211042010027\n");
+    for(int f=1;f<101;f++)
+    {
+            if(f%7==0||f%10==7)
+            printf("%d\n",f);
+    }
+}
+  
