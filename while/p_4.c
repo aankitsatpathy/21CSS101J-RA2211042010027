3a6dbc45615117c6ca194c426ae08d186001bd2b
aankitsatpathy <113981163+aankitsatpathy@users.noreply.github.com>
2022-11-11 03:54:07 +0530

    Create p_4.c

---
+++ while/p_4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("RA2211042010027\n");
+    int a=5,n;
+    scanf("%d",&n);
+    while(a<=n)
+    {
+        printf("%d\n",a);
+        a+=5;
+    }
+}
