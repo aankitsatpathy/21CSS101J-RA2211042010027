49b6e048afa4474a65aa314049ea452c61ea5aa2
aankitsatpathy <113981163+aankitsatpathy@users.noreply.github.com>
2022-11-11 03:59:15 +0530

    Create p_6.c

---
+++ while/p_6.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+
+int main()
+{
+    printf("RA2211042010027\n");
+    int a=0,n;
+    scanf("%d",&n);
+    while(a++<=n)
+    {
+        if(a%2==0)
+        printf("%d\n",a);
+    }
+}
