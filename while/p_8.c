78a286712f573aece3691ecf4ae7ee5068080b06
aankitsatpathy <113981163+aankitsatpathy@users.noreply.github.com>
2022-11-11 04:03:23 +0530

    Create p_8.c

---
+++ while/p_8.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+
+int main()
+{
+    printf("RA2211042010027\n");
+    int n,a=1;
+    scanf("%d",&n);
+    while(a<11)
+    {
+       printf("%d\n",n*a);
+       a++;
+    }
+}
