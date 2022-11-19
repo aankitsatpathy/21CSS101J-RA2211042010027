13dc9f7e313f828fe40cc222bf1dc5451ee8cf03
aankitsatpathy <113981163+aankitsatpathy@users.noreply.github.com>
2022-11-20 00:09:26 +0530

    Create p_5.c

---
+++ dowhile/p_5.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+void main()
+{
+    printf("RA2211042010027\n");
+    printf("Enter a number");
+    int n,s=0,i=0;
+    scanf("%d",&n);
+    do{
+        s+=i;
+        i++;
+    }while(i<=n);
+    printf("Sum = %d",s);
+}
