1378bdf070a2a897b8203112a521ce14f046659f
aankitsatpathy <113981163+aankitsatpathy@users.noreply.github.com>
2022-11-20 00:07:16 +0530

    Create p_4.c

---
+++ dowhile/p_4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main()
+{
+    printf("RA2211042010027\n");
+    printf("Enter a number");
+    int n,i=0;
+    scanf("%d",&n);
+    do{
+        if(i%2!=0)
+        printf("%d\n",i);
+        i++;
+    }while(i<=n);
+}
