2375e4b7c20e8248fba674ea0d7b4052cf3b507e
aankitsatpathy <113981163+aankitsatpathy@users.noreply.github.com>
2022-11-20 01:22:30 +0530

    Create p_6.c

---
+++ array/p_6.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("RA2211042010027\n");
+    int s=1,a[2][2];
+    for(int i=0;i<2;i++){
+    for(int j=0;j<2;j++)
+    scanf("%d",&a[i][j]);
+    printf("\n");}
+    for(int i=0;i<2;i++){
+        s=1;
+    for(int j=0;j<2;j++){
+    s*=a[i][j];
+    printf("%d\n",s);}
+    printf("\n");}
+}
