c0de0f1830e7aa51b0ee785e84c2038844f771e8
aankitsatpathy <113981163+aankitsatpathy@users.noreply.github.com>
2022-11-20 01:13:47 +0530

    Create p_1.c

---
+++ array/p_1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("RA2211042010027\n");
+    int s=0,a[10];
+    for(int i=0;i<10;i++)
+    scanf("%d",&a[i]);
+    for(int i=0;i<10;i++)
+    printf("%d\n",a[i]);
+}
