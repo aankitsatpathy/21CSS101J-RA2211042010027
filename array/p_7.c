d0f95ddaf50f94be652813e9e6ae948665210ab5
aankitsatpathy <113981163+aankitsatpathy@users.noreply.github.com>
2022-11-20 01:29:36 +0530

    Create p_7.c

---
+++ array/p_7.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("RA2211042010027\n");
+    int a[10];
+    for(int i=0;i<10;i++){
+    scanf("%d",&a[i]);
+    }
+    int n,p=-1;
+    printf("Enter search number");
+    scanf("%d",&n);
+    for(int i=0;i<10;i++){
+        if(a[i]==n)
+        p=i;
+    }
+    printf("%d",p);
+}
