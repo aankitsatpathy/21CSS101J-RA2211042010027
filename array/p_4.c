42d1edb4bd613072a5ed0ec5ccfb4657c6e9a347
aankitsatpathy <113981163+aankitsatpathy@users.noreply.github.com>
2022-11-20 01:25:50 +0530

    Create p_4.c

---
+++ array/p_4.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+int main()
+{
+    printf("RA2211042010027\n");
+    int a[2][2];
+    for(int i=0;i<2;i++){
+    for(int j=0;j<2;j++)
+    scanf("%d",&a[i][j]);
+    printf("\n");}
+    for(int i=0;i<2;i++){
+    for(int j=0;j<2;j++){
+    printf("%d\t",a[i][j]);}
+    printf("\n");}
+}
