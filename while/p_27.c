c141fc87d64d70380d5a05d9f6e2b01aa7fccccd
aankitsatpathy <113981163+aankitsatpathy@users.noreply.github.com>
2022-11-19 22:52:34 +0530

    Create p_27.c

---
+++ while/p_27.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void main()
+{
+    printf("RA2211042010027\n");
+    printf("Enter a number");
+    int n;
+    scanf("%d",&n);
+     int a[10],n;  
+    printf("Enter the number");
+    scanf("%d",&n);
+    int i=0;
+    while(n>0)
+    {
+        a[i]=n%2;
+        n=n/2;
+        i++;
+    }  
+    int c=0;
+    i=0;
+}
