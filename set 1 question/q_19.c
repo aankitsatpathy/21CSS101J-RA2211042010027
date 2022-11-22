379908c6d6de12f68cca04e4fd7e957a70bbd4b3
aankitsatpathy <113981163+aankitsatpathy@users.noreply.github.com>
2022-11-23 00:13:18 +0530

    Create q_19.c

---
+++ set 1 question/q_19.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+void main()
+{
+    printf("Enter bike number : ");
+    int n,t,a=0;
+    scanf("%d",&n);
+    t=n;
+    while(n>0)
+    {
+        int d=n%10;
+        if(d==1||d==0||d==2||d==3||d==5||d==8)
+        {
+            a++;
+        }n/=10;
+    }
+    if(a>0)
+    {
+        printf("Bike Number has fibonacci number");
+    }
