b8063207d036796d4b9df01f867a8ba4867667f0
aankitsatpathy <113981163+aankitsatpathy@users.noreply.github.com>
2022-11-23 00:19:38 +0530

    Create q_4.c

---
+++ set 2 question/q_4.c
@@ -0,0 +1,17 @@
+//QUESTION 4
+#include <stdio.h>
+
+int main()
+{
+    int n,x,y;
+    printf("Enter N,X,Y : ");
+    scanf("%d %d %d",&n,&x,&y);
+    if(y%x==0&&y<=(n*x))
+    {
+    printf("YES");
+    }
+    else
+    {
+    printf("NO");
+    }
+}
