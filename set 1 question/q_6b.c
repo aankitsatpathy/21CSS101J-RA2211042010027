bbd878dadea0d388be1a26d4bc9b1774341051cc
aankitsatpathy <113981163+aankitsatpathy@users.noreply.github.com>
2022-11-23 00:12:40 +0530

    Create q_6b.c

---
+++ set 1 question/q_6b.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+void main()
+{
+    int a;
+    float c=0;
+    printf("Enter a : ");
+    scanf("%d ",&a);
+    int b=a>250?1:a>150?2:a>50?3:4;
+    switch(b)
+    {
+        case 1:
+        c+=(a-250)*1.5;
+        a=250;
+        case 2:
+        c+=(a-150)*1.2;
+        a=150;
+        case 3:
+        c+=(a-50)*0.75;
+        case 4:
+        c+=a*0.75;
+    }
+    if(c>300)
+    {
+        c+=(c*0.1);
+    }
+   printf("Cost = %f\n",c);
+}
