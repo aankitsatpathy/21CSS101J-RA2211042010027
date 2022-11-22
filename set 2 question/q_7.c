0c2040914df4e21fca217c4efeeb675bb596c11c
aankitsatpathy <113981163+aankitsatpathy@users.noreply.github.com>
2022-11-23 00:25:23 +0530

    Create q_7.c

---
+++ set 2 question/q_7.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("Enter the number of rows : ");
+    scanf("%d", &n );
+
+    for (int i = n; i > 0  ; i--)
+    {
+        for (int j = i; j > 0; j--)
+    {
+        printf("%d\t", i);
+    }
+    printf("\n");
+    }
+}
