7663870f6e379d3148c2cc545a3fc85ba9b98111
aankitsatpathy <113981163+aankitsatpathy@users.noreply.github.com>
2022-11-23 00:27:03 +0530

    Create q_19.c

---
+++ set 2 question/q_19.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void swap(int *a, int *b);
+
+
+int main()
+{
+    int A, B;
+    scanf("%d %d", &A,&B);
+    int *ptr1 = &A;
+    int *ptr2 = &B;
+
+    swap(&A,&B);
+
+    printf("%d %d", A, B);
+}
+
+void swap(int*a, int*b)
+{
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
