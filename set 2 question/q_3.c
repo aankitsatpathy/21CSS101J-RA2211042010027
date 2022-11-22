48d088a6ad5036ca576e73d16c9f53259072ed92
aankitsatpathy <113981163+aankitsatpathy@users.noreply.github.com>
2022-11-23 00:18:46 +0530

    Create q_3.c

---
+++ set 2 question/q_3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    //declaring the variables
+    int N, M;
+    //finding the number of friends
+    printf("Number of friends: ");
+    scanf("%d", &N);
+    //finding the number of left shoes
+    printf("Number of left shoes: ");
+    scanf("%d", &M);
+    //Output
+    printf("Number of extra pairs of shoes to be bought: %d\n", N-M);
+    printf("Number of right shoes to be bought: %d\n", M);
+}
