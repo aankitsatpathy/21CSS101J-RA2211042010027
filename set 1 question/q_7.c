780e2d8c24581c444fbb50919daf7167b899b016
aankitsatpathy <113981163+aankitsatpathy@users.noreply.github.com>
2022-11-23 00:06:48 +0530

    Create q_7.c

---
+++ set 1 question/q_7.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+void main()
+{
+    int c;
+    printf("Enter \n1. Total Amount\n2. Balance to be paid\n3. Balance to be recieved\n");
+    scanf("%d",&c);
+    switch(c)
+    {
+        case 1:
+        printf("Total Amount = 6000");
+        break;
+        case 2:
+        printf("Balance to be paid = 1000");
+        break;
+        case 3:
+        printf("Balance to be received = 4000");
+    }
+}
